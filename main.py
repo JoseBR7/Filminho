d25afa92ebc4222725ef37adaf9fc39e1e84eb18
JoseBR7 <josearthurmoraiscorrea3@gmail.com>
2025-09-27 09:36:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 
+url_movies = "https://drive.google.com/uc?export=download&id=17dWfqGAtdKZAR0rTCT6cv7weiIcrNycZ"
+    url_credits = "https://drive.google.com/uc?export=download&id=1hQwFfz4ZXtF9UYwiH7VEwThbg207XjRL"
