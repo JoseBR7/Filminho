d989c83495e9d813a9fa931e0fcc3b5660db056d
JoseBR7 <josearthurmoraiscorrea3@gmail.com>
2025-09-27 09:37:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-
-url_movies = "https://drive.google.com/uc?export=download&id=17dWfqGAtdKZAR0rTCT6cv7weiIcrNycZ"
+def carregar_dados():
+    url_movies = "https://drive.google.com/uc?export=download&id=17dWfqGAtdKZAR0rTCT6cv7weiIcrNycZ"
     url_credits = "https://drive.google.com/uc?export=download&id=1hQwFfz4ZXtF9UYwiH7VEwThbg207XjRL"
+
+    movies_df = pd.read_csv(url_movies)
+    credits_df = pd.read_csv(url_credits, engine='python', on_bad_lines='skip')
+    df = movies_df.merge(credits_df, left_on="id", right_on="movie_id")
+    df.drop("movie_id", axis=1, inplace=True)
+    return df
+
+df = carregar_dados()
